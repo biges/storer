2a074e25a70b410e6243a406b478716ea90fd8cf
Ahmet AYGÜN <me@ahmetaygun.net>
2018-08-01 12:07:36 +0300

    generic interface added

---
+++ storer.go
@@ -0,0 +1,25 @@
+package storer
+
+import (
+	"errors"
+)
+
+// ErrNotFound will be returned for any of not found errors
+var ErrNotFound = errors.New("not found")
+
+// Storer defines common data storage functions
+type Storer interface {
+	Find(table string, query interface{}, result interface{}, pagination *PaginationParams) error
+	Create(table string, object interface{}) error
+	Update(table string, query interface{}, change interface{}) error
+	Delete(table string, query interface{}) error
+	Close() error
+	NewPaginationParams() *PaginationParams
+}
+
+// PaginationParams should've used to pass pagination parameters to data layer
+type PaginationParams struct {
+	Limit  int
+	SortBy string
+	Page   int
+}
